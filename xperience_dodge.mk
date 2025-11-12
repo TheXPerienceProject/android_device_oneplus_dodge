aed55cec7adad3bd7dd164cba31a4d3ae21626e1
klozz <carlosj@klozz.dev>
2025-11-11 23:41:03 -0600

    dodge: XPerience bringup
    Change-Id: I57044c145dfb24b19bd79ef54c95aa26e3cd955f
    Signed-off-by: klozz <carlosj@klozz.dev>

---
+++ xperience_dodge.mk
@@ -0,0 +1,31 @@
+#
+# Copyright (C) 2021-2025 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit_only.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit from dodge device
+$(call inherit-product, device/oneplus/dodge/device.mk)
+
+# Inherit some common Lineage stuff.
+$(call inherit-product, vendor/xperience/config/common.mk)
+
+PRODUCT_NAME := xperience_dodge
+PRODUCT_DEVICE := dodge
+PRODUCT_MANUFACTURER := OnePlus
+PRODUCT_BRAND := OnePlus
+PRODUCT_MODEL := CPH2653
+
+PRODUCT_GMS_CLIENTID_BASE := android-oneplus
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    BuildDesc="qssi_64-user 15 AP3A.240617.008 1756201541239 release-keys" \
+    BuildFingerprint=OnePlus/CPH2653EEA/OP5D55L1:15/AP3A.240617.008/V.R4T3.38e5dcd-19631d6-1977a50:user/release-keys \
+    DeviceName=OP5D55L1 \
+    DeviceProduct=CPH2653 \
+    SystemDevice=OP5D55L1 \
+    SystemName=CPH2653
