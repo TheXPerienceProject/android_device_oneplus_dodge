f114c70af2c83ccbca6bf64074977f47e12f544f
John Galt <johngaltfirstrun@gmail.com>
2026-01-13 12:18:43 -0500

    dodge: allow permissive bitclk rates
    Change-Id: Ic1fc75e760aee1a6bbfe7dc3f7279613ea362cc1
    Signed-off-by: klozz <carlosj@klozz.dev>

--- device.mk
+++ device.mk
@@ -18,6 +18,7 @@ TARGET_SCREEN_HEIGHT := 3168
 TARGET_SCREEN_WIDTH := 1440
 
 # Display
+$(call soong_config_set,qtidisplay,bitclk_rate_permissive,true)
 $(call soong_config_set,surfaceflinger,frame_rate_category_high,120)
 $(call soong_config_set,surfaceflinger,frame_rate_category_min,1)
 
